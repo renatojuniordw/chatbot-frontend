47ed8c9e37ec46b70adabfe2ed210fefb3f57380
Renato Bezerra <renato-bgs@live.com>
2025-05-22 21:07:43 -0300

    Add new media files, Docker configuration, and update frontend setup
    - Added new audio and image files for chat notifications and branding.
    - Introduced a Docker Compose configuration for frontend development.
    - Updated package.json to allow starting the React app on all network interfaces.
    - Created new HTML and manifest files for improved web app support.
    - Added server-side static file serving for the production build of the frontend.

---
+++ build/precache-manifest.8cdcc464e72517c8bd6636cb3adbd6f5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b728a8f5deb97ffd45ac553044bf1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "041ece42ece042b9393d",
+    "url": "/static/css/2.fbde4ecd.chunk.css"
+  },
+  {
+    "revision": "041ece42ece042b9393d",
+    "url": "/static/js/2.20be5070.chunk.js"
+  },
+  {
+    "revision": "93d984260bac9b30c33efb37e165440e",
+    "url": "/static/js/2.20be5070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1cb0c6ee7ef40a9eeda",
+    "url": "/static/js/3.f1a786eb.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/3.f1a786eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af12ee3117999e56462f",
+    "url": "/static/js/main.3d6155c4.chunk.js"
+  },
+  {
+    "revision": "146fc893931e529ab95f",
+    "url": "/static/js/runtime-main.42fd01a9.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "5c788b8d54d3ebf021114528116dfd77",
+    "url": "/static/media/n8n.5c788b8d.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.8244f042.png"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  },
+  {
+    "revision": "e5374d73ff43538fdaa16a3f1a980f4b",
+    "url": "/static/media/webhook.e5374d73.png"
+  }
+]);
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-//simple express server to run frontend production build;
 const express = require("express");
+// const cors = require("cors");
 const path = require("path");
+
 const app = express();
+
 app.use(express.static(path.join(__dirname, "build")));
+
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(3000);
 
+app.listen(3000);
