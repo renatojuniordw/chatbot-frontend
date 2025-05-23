2edfa0b95233024a9add88ac21d91d7c22fa3fb8
Renato Bezerra <renato-bgs@live.com>
2025-05-22 23:10:37 -0300

    Update port configuration to 3010 in Dockerfile, docker-compose.yml, and server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
-// const cors = require("cors");
 const path = require("path");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", function (req, res) {
-	res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
-app.listen(3000);
+const PORT = 3010;
+
+app.listen(PORT, () => {
+	console.log(`Servidor rodando na porta ${PORT}`);
+});
